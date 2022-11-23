9b97be44405f95e0f4eb1641dbc5eb0de7758094
Стас Басов <stas.basov@whatever.com>
2022-11-23 17:07:36 +0600

    rev1.05

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,10 +1,7 @@
 import datetime
 import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
+# from dotenv import load_dotenv
+# load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
